b6fa19cf2ee5ea685475c26794ec665bdb871cdc
DeniArif <deniarifwibowo82@gmail.com>
2023-06-09 09:55:46 +0700

    comit

--- ExeraiseLinkedlist_188/ExeraiseLinkedlist_188.cpp
+++ ExeraiseLinkedlist_188/ExeraiseLinkedlist_188.cpp
@@ -3,23 +3,19 @@
 using namespace std;
 
 class Node {
-public: 
-	string rollNumber;
+public:
+	int rollNumber;
 	string name;
 	Node* next;
 };
-
 class CircularLinkedList {
 private:
 	Node* LAST;
-
 public:
 	CircularLinkedList() {
 		LAST = NULL;
-	}
-
-	void addNode();
-	bool search(int rollno, Node** previous, Node** current);
+	}void addNode();
+	bool search(int rollno, Node** Dio, Node** Arya);
 	bool listEmpty();
 	bool delNode();
 	void traverse();
@@ -28,7 +24,7 @@ public:
 void CircularLinkedList::addNode() {
 	int nim;
 	string na;
-	cout << "\nEnter thr rollnumber of the student: ";
+	cout << "\nEnter the rollnumber of the student: ";
 	cin >> nim;
 	cout << "\nEnter the name of the student: ";
 	cin >> na;
@@ -36,15 +32,27 @@ void CircularLinkedList::addNode() {
 	newNode->rollNumber = nim;
 	newNode->name = na;
 
-}
 
-bool CircularLinkedList::search(int roolno, Node** previous, Node** currentO) {
-	*previous = LAST->next;
-	*current = LAST->next;
-
-	while (current != LAST) {
-		if (rollno == (*current)->rollNumber) {
-			return true;
+	if (LAST == NULL || nim <= LAST->rollNumber) {
+		if (LAST != NULL && nim == LAST->rollNumber) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
 		}
+		newNode->next = LAST;
+		if (LAST != NULL)
+			LAST->rollNumber = newNode;
+		newNode->rollNumber = NULL;
+		LAST = newNode;
+		return;
 	}
-}
+	bool CircularLinkedList::search(int roolno, Node * *previous, Node * *currentO) {
+		*previous = LAST->next;
+		*current = LAST->next;
+
+		while (current != LAST) {
+			if (rollno == (*current)->rollNumber) {
+				return true;
+			}
+		}
+
+	}
